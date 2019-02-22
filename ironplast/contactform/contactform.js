3773e1975a92242c814bb23e5a53d031d4f40d51
guidohernan93 <guidohernan93@gmail.com>
2019-02-22 16:50:57 -0300

    Add files via upload
    Uploaded project

---
+++ ironplast/contactform/contactform.js
@@ -0,0 +1,30 @@
+jQuery(function($)  
+{
+    $("#contact_form").submit(function()
+    {
+		var data = {
+			name: $("#name").val(),
+			email: $("#email").val(),
+			message: $("#msg").val(),
+			subject: $("#subject").val()
+		};
+		
+        $.ajax(
+        {
+            type: "POST",
+            url: "http://127.0.0.1/ironplast/send_email.php",
+            data: data
+        })
+        .done(function(response) {
+            alert('Your message has been sent. Thank you!'); // show success message
+            $("#name").val(''); // reset field after successful submission
+            $("#email").val(''); // reset field after successful submission
+            $("#msg").val(''); // reset field after successful submission
+			$("#subject").val(''); // reset field after successful submission
+        })
+        .fail(function(response) {
+            alert('Error sending message.');
+        });
+        return false; // prevent page refresh
+    });
+});
